60523737f6b2d3755e8e45b3756d9722f0ed7798
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-10-22 12:04:05 -0500

    using the console

---
+++ app.js
@@ -0,0 +1,29 @@
+// Log to console
+
+console.log('Hello World');
+console.log(123);
+console.log(true);
+
+var greeting = 'Larry';
+console.log(greeting);
+console.log([1 ,2, 3, 4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error('This is an error')
+console.clear();
+console.warn('This is a warning');
+console.time('Time');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.timeEnd('Time');
+
+/*
+multi-line
+comments
+*/
+
