ddf343ebff0fa5f3bc2b89d274a2cde6220ffcd0
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-10-22 12:24:27 -0500

    Variables - Var, Let, and Const

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
-// Log to console
+// //var, let, const
 
-console.log('Hello World');
-console.log(123);
-console.log(true);
+// var fullName = 'John Doe';
+// console.log(fullName);
+// fullName = 'Larry Rhodes';
+// console.log(fullName);
 
-var greeting = 'Larry';
-console.log(greeting);
-console.log([1 ,2, 3, 4]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
-console.error('This is an error')
-console.clear();
-console.warn('This is a warning');
-console.time('Time');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.log('Hello World');
-console.timeEnd('Time');
+// //Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
 
-/*
-multi-line
-comments
-*/
+// //letters, numbers, _, $
+// // Can not start with number
+
+// var firstName = 'John'; //Camel case
+// var first_name = 'Sara'; //Underscore
+// var FirstName = 'Tom'; //Pascal case 
+// var firstname; //not recommended
+
+// //LET 
+// let name;
+// let fullName = 'John Doe';
+// console.log(fullName);
+// fullName = 'Larry Rhodes';
+// console.log(fullName);
+
+//CONST
+
+// const firstName ='John';
+// console.log(firstName);
+// // Can't reassign
+// // firstName ='Sara';
+// // Have to assign a value
+// // const greeting;
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Sara'
+console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+
+console.log(numbers[2]);
+
+console.log(numbers);
+
+
 
