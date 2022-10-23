e93edcbfe6965e5119581ccda3fd592d288ac27a
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-10-22 19:20:43 -0500

    Data Types in JavaScript

--- app.js
+++ app.js
@@ -1,54 +1,15 @@
-// //var, let, const
+//PRIMITIVE
 
-// var fullName = 'John Doe';
-// console.log(fullName);
-// fullName = 'Larry Rhodes';
-// console.log(fullName);
+//String
+const fullName ='John Doe';
 
-// //Init var
-// var greeting;
-// console.log(greeting);
-// greeting = 'Hello';
-// console.log(greeting);
+//Number
+const age =45;
 
-// //letters, numbers, _, $
-// // Can not start with number
+//Boolean
+const hasKids = true;
 
-// var firstName = 'John'; //Camel case
-// var first_name = 'Sara'; //Underscore
-// var FirstName = 'Tom'; //Pascal case 
-// var firstname; //not recommended
-
-// //LET 
-// let name;
-// let fullName = 'John Doe';
-// console.log(fullName);
-// fullName = 'Larry Rhodes';
-// console.log(fullName);
-
-//CONST
-
-// const firstName ='John';
-// console.log(firstName);
-// // Can't reassign
-// // firstName ='Sara';
-// // Have to assign a value
-// // const greeting;
-
-const person = {
-  name: 'John',
-  age: 30
-}
-
-person.name = 'Sara'
-console.log(person);
-
-const numbers = [1, 2, 3, 4, 5];
-numbers.push(6);
-
-console.log(numbers[2]);
-
-console.log(numbers);
+console.log(typeof hasKids);
 
 
 
