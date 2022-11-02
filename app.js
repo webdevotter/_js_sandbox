de7392fef76eab918447d5be45229a57a06e8862
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-02 08:10:11 -0500

    Type Conversion

--- app.js
+++ app.js
@@ -19,9 +19,29 @@ val = String([1,2,3,4]);
 val = (5).toString();
 val = (true).toString();
 
+// String to number
+val = Number('5'); 
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1,2,3]);
+
+val = parseInt('100.30'); //Integer
+val = parseFloat('100.30'); //Decimals
+
+
 //Output
-console.log(val);
-console.log(typeof val);
-console.log(val.length);
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// console.log(val.toFixed(2));
+
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
 
 
