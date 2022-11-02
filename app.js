87a4b1e7a6b7fe6e6b3f34bc921e8108317f0b2b
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-02 08:52:08 -0500

    String Methods & Concatenation

--- app.js
+++ app.js
@@ -17,6 +17,38 @@ val += 'Traversy'
 
 val = 'Hell, my name is ' + firstName + ' and I am ' + age;
 
+// Escaping
+
+val = 'That\'s awesome, I can\'t wait.';
+val = "That's awesome, I can't wait."; // same as previous
+
+// Length
+
+val = firstName.length;
+val = lastName.length;
+
+// concat
+
+val = firstName.concat(' ', lastName); // same as line 11
+
+// Change case
+
+val = firstName.toUpperCase();
+val = lastName.toLowerCase();
+
+
+val = firstName[1];
+
+// indexOf()
+
+val = firstName.indexOf('L');
+val = firstName.lastIndexOf('y');
+
+//charAt()
+
+val = firstName.charAt('2');
+
+
 console.log(val);
 
 
