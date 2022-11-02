0958932cd6ee0dc0a7b8c0428379e51c663dbc19
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-01 20:18:46 -0500

    Type Conversion

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ val = String(new Date());
 //Array to string
 val = String([1,2,3,4]);
 
+// toString()
+
+val = (5).toString();
+val = (true).toString();
+
 //Output
 console.log(val);
 console.log(typeof val);
