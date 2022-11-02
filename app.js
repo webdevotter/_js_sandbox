696e8534cc4ba8da69890c508aa46891746f6e65
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-02 08:20:19 -0500

    Numbers & The Math Object

--- app.js
+++ app.js
@@ -1,47 +1,30 @@
-let val;
+const num1 = 100;
+const num2 = 50;
+let val; //Initialize (no value set yet)
 
-//Number to string
+// Simple math with numbers
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2; // returns remainder
 
-val = String(555);
-val = String(4+4);
+// Mtah Object
 
-//Bool to string
-val = String(true);
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8); //round
+val = Math.ceil(2.3); //round up
+val = Math.floor(2.8); // round down
+val = Math.sqrt(4);
+val = Math.abs(-3); //positive value
+val = Math.pow(8, 2); // first to power of second
+val = Math.min(2, 33, 4, 1, 66, 6, 3, -2); // returns min number
+val = Math.max(2, 33, 4, 1, 66, 6, 3, -2); // returns max number
+val = Math.random(); //MDN: returns a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1, with approximately uniform distribution over that range — which you can then scale to your desired range
 
-//Date to string
-val = String(new Date());
+val = Math.floor(Math.random() * 20 + 1); // random number between 1 and 20
 
-//Array to string
-val = String([1,2,3,4]);
-
-// toString()
-
-val = (5).toString();
-val = (true).toString();
-
-// String to number
-val = Number('5'); 
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number('hello');
-val = Number([1,2,3]);
-
-val = parseInt('100.30'); //Integer
-val = parseFloat('100.30'); //Decimals
-
-
-//Output
-// console.log(val);
-// console.log(typeof val);
-// console.log(val.length);
-// console.log(val.toFixed(2));
-
-const val1 = String(5);
-const val2 = 6;
-const sum = Number(val1 + val2);
-
-console.log(sum);
-console.log(typeof sum);
+console.log(val);
 
 
