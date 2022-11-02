3cac65baaa718749db904955e6b501f78320c202
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-02 08:41:36 -0500

    String Methods & Concatentation

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-const num1 = 100;
-const num2 = 50;
-let val; //Initialize (no value set yet)
+const firstName = 'Larry'
+const lastName = 'Rhodes'
+const age = 39;
 
-// Simple math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2; // returns remainder
+let val;
 
-// Mtah Object
+val = firstName + lastName;
 
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.8); //round
-val = Math.ceil(2.3); //round up
-val = Math.floor(2.8); // round down
-val = Math.sqrt(4);
-val = Math.abs(-3); //positive value
-val = Math.pow(8, 2); // first to power of second
-val = Math.min(2, 33, 4, 1, 66, 6, 3, -2); // returns min number
-val = Math.max(2, 33, 4, 1, 66, 6, 3, -2); // returns max number
-val = Math.random(); //MDN: returns a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1, with approximately uniform distribution over that range — which you can then scale to your desired range
+// Concatenation 
 
-val = Math.floor(Math.random() * 20 + 1); // random number between 1 and 20
+val = firstName +' '+ lastName;
+
+// Append: add to, not replace
+
+val = 'Brad ';
+val += 'Traversy'
+
+val = 'Hell, my name is ' + firstName + ' and I am ' + age;
 
 console.log(val);
 
