547baa0088b2bca1af1eedb77520431261de0e8a
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-03 07:14:32 -0500

    Arrays & Array Methods

--- app.js
+++ app.js
@@ -1,42 +1,92 @@
-const firstName = 'Larry';
-const age = 39;
-const job = 'Mortgage Banker';
-const city = 'Cape Girardeau';
-const pets = ['Maggie', 'Brady', 'Bear'];
+// Create Arrays
 
-let html;
+const numbers = [81, 65, 70, 55, 30, 100, 99];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+const mixed = [2, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
 
-// Without template strings (es5)
+let val;
 
-html = `<ul><li>Name: `+ firstName +` </li><li>Age: `+ age +`</li><li>Job: `+ job+`</li><li>City: `+ city +`</li></ul>`;
+//Array Length
 
-html =`<ul>` +
-      `<li>Name: `+ firstName +` </li>` +
-      `<li>Name: `+ age +` </li>` +
-      `<li>Name: `+ job +` </li>` +
-      `<li>Name: `+ city +` </li>`;
-      '</ul>'
+val = numbers.length;
 
 
-// With template strings (es6)
+// Check if is array
 
+val = Array.isArray(numbers);
 
-function hello() {
-  return 'Hello,' + ' ' + firstName + '!';
+// Get single value
+
+val = numbers[3];
+val = numbers[0];
+
+//Insert into array
+numbers[0] = 1000;
+
+val = numbers[0];
+
+// Find index of value
+
+val = numbers.indexOf(9);
+
+// Mutating arrays
+
+//add to
+
+numbers.push(250);
+
+// add to front
+
+numbers.unshift(120);
+
+// Take off from end
+
+numbers.pop();
+
+// Take off from front
+
+numbers.shift();
+
+//Splice values
+
+numbers.splice(0, 1); // start index and end index
+
+// Reverse
+
+numbers.reverse();
+
+// Concatenate 
+
+val = numbers.concat(fruit);
+
+// Sort
+
+val = fruit.sort();
+val = numbers.sort();
+
+// Use the "compare function"
+
+val = numbers.sort(function(x, y){
+  return x -y;
+})
+
+// Reverse sort
+ 
+val = numbers.sort(function(x, y){
+  return y -x;
+})
+
+// Find
+
+function under50(num) {
+   return num < 50;
 }
 
-html = `
-<ul>
-  <li>Name: ${firstName} </li>
-  <li>Age:  ${age} </li>
-  <li>Job: ${job} </li>
-  <li>City: ${city} </li>
-  <li>Pets: ${pets.length}</li>
-  <li>Function: ${hello()} </li>
-  <li>Age Is: ${age > 30 ? 'Over 30' : 'Under 30'} </li>
-</ul>
-`
+val = numbers.find(under50);
 
-document.body.innerHTML = html;
+
+console.log(val);
+console.log(numbers);
 
 
