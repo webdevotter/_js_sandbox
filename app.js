78a058a72dc9e9795f5004b0d1410118c641a339
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-05 07:56:58 -0500

    Functions declarations & expressions

--- app.js
+++ app.js
@@ -27,4 +27,21 @@ const square = function(x = 3){
 //   console.log('Hello ' + firstName);
 // })('Brad');
 
+const todo = {
+  add: function(){
+    console.log('Add todo...');
+  },
+  edit: function(id){
+      console.log(`Edit todo ${id}`);
+  }
+}
+
+todo.delete = function() {
+  console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(88);
+todo.delete();
+
 
