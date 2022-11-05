935d9bdc74af999668f0be88703fdad51d77272a
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-05 08:12:28 -0500

    General Loops: While Loop

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-// FOR LOOP
+// // FOR LOOP
 
-for(let i = 0; i < 10; i++){
-  //console.log('Number: ' + i);
-  if(i === 2){
-    console.log( `${i} `+ 'is my favorite number');
-    continue;
-  }
-  if(i === 5){
-    console.log('Stop the loop');
-    break;
-  }
-  console.log('Number: ' + i);
+// for(let i = 0; i < 10; i++){
+//   //console.log('Number: ' + i);
+//   if(i === 2){
+//     console.log( `${i} `+ 'is my favorite number');
+//     continue;
+//   }
+//   if(i === 5){
+//     console.log('Stop the loop');
+//     break;
+//   }
+//   console.log('Number: ' + i);
+// }
+
+//WHILE LOOP
+
+let i = 0;
+
+while(i < 10){
+  console.log('Number ' + i);
+  i++;
 }
 
 
