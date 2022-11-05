bc0d6c9e7c41bfbdbab0c31214f9bcf37abb6bbb
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-04 20:28:13 -0500

    if statements & comparison operators

--- app.js
+++ app.js
@@ -1,40 +1,69 @@
-// if(something) {
-//   do something
+// // if(something) {
+// //   do something
+// // } else {
+// //   do something else
+// // }
+
+const id = '200';
+
+// // Equal To
+
+// if(id == 100){
+//   console.log('CORRECT');
 // } else {
-//   do something else
+//   console.log('INCORRECT');
 // }
 
-const id = '100';
+// // Not Equal to
 
-// Equal To
+// if(id != 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
 
-if(id == 100){
-  console.log('CORRECT');
+// // Equal to Value and Type
+
+// if(id === 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // Not equal to Value and Type
+
+// if(id !== 100){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+
+// Test if undefined
+
+// if(typeof id !== 'undefined'){
+//   console.log(`The ID is ${id}`);
+// } else {
+//   console.log('NO ID');
+// }
+
+//Greater or less than
+
+// if(id >= 200){
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// IF ELSE
+
+const color = 'blue';
+
+if(color === 'red'){
+  console.log('Color is red');
+} else if (color === 'blue'){
+  console.log('Color is blue');
 } else {
-  console.log('INCORRECT');
-}
-
-// Not Equal to
-
-if(id != 100){
-  console.log('CORRECT');
-} else {
-  console.log('INCORRECT');
-}
-
-// Equal to Value and Type
-
-if(id === 100){
-  console.log('CORRECT');
-} else {
-  console.log('INCORRECT');
-}
-
-// Not equal to Value and Type
-
-if(id !== 100){
-  console.log('CORRECT');
-} else {
-  console.log('INCORRECT');
+  console.log('Color is neither');
 }
 
