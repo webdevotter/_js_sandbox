1be393764d59034ac3bd951c7931a7e509bae6bd
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-05 07:53:55 -0500

    Function Declarations & Expressions

--- app.js
+++ app.js
@@ -1,42 +1,30 @@
-const color = 'yellow';
+// FUNCTION DECLARATIONS
 
-switch(color){
-  case 'red':
-    console.log('Color is red');
-    break;
-  case 'blue':
-    console.log('Color is blue');
-    break;
-  default:
-    console.log('Color is not red or blue');
-    break;
+function greet(firstName = 'Betty', lastName = 'Boop'){
+  // if(typeof firstName === 'undefined'){firstName = 'Betty'}
+  // if(typeof lastName === 'undefined'){lastName = 'Boop'}
+  // console.log('Hello');
+  return 'Hello ' +firstName+ ' '+lastName;
 }
 
-let day;
+// console.log(greet('Larry', 'Rhodes'));
 
-switch(new Date().getDay()){
-  case 0:
-    day = 'Sunday';
-    break;
-  case 1:
-    day = 'Monday';
-    break;
-  case 2:
-    day = 'Tuesday';
-    break;
-  case 3:
-    day = 'Wednesday';
-    break;
-  case 4:
-    day = 'Thursday';
-    break;
-  case 5:
-    day = 'Friday';
-    break;
-  case 6:
-    day = 'Saturday';
-    break;
+//FUNCTION EXPRESSIONS
+
+const square = function(x = 3){
+  return x * x;
 }
 
-console.log(`Today is ${day}`);
+// console.log(square(8));
+
+// IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
+
+// (function(){
+//   console.log('IIFE Ran...');
+// })();
+
+// (function(firstName){
+//   console.log('Hello ' + firstName);
+// })('Brad');
+
 
