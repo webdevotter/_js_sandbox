a7ef5ea353fe0d859200804bf05da49367444035
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-05 08:09:09 -0500

    General Loops : For Loop

--- app.js
+++ app.js
@@ -1,47 +1,16 @@
-// FUNCTION DECLARATIONS
+// FOR LOOP
 
-function greet(firstName = 'Betty', lastName = 'Boop'){
-  // if(typeof firstName === 'undefined'){firstName = 'Betty'}
-  // if(typeof lastName === 'undefined'){lastName = 'Boop'}
-  // console.log('Hello');
-  return 'Hello ' +firstName+ ' '+lastName;
-}
-
-// console.log(greet('Larry', 'Rhodes'));
-
-//FUNCTION EXPRESSIONS
-
-const square = function(x = 3){
-  return x * x;
-}
-
-// console.log(square(8));
-
-// IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
-
-// (function(){
-//   console.log('IIFE Ran...');
-// })();
-
-// (function(firstName){
-//   console.log('Hello ' + firstName);
-// })('Brad');
-
-const todo = {
-  add: function(){
-    console.log('Add todo...');
-  },
-  edit: function(id){
-      console.log(`Edit todo ${id}`);
+for(let i = 0; i < 10; i++){
+  //console.log('Number: ' + i);
+  if(i === 2){
+    console.log( `${i} `+ 'is my favorite number');
+    continue;
   }
+  if(i === 5){
+    console.log('Stop the loop');
+    break;
+  }
+  console.log('Number: ' + i);
 }
-
-todo.delete = function() {
-  console.log('Delete todo...');
-}
-
-todo.add();
-todo.edit(88);
-todo.delete();
 
 
