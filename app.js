d0ff4515f26fb0bc28b6fb05d45cf56fb976cc70
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-06 12:35:20 -0600

    Window Object:
    Scroll, Location, Redirect, Reload

--- app.js
+++ app.js
@@ -2,21 +2,49 @@
 
 
 // Alert
-
 // alert('Hello World');
 
 // Prompt
-
 // const input = prompt();
-
 // alert(input);
 
 // Confirm
-if(confirm('Are you sure')){
-  console.log('YES');
-} else {
-  console.log('NO');
-}
+// if(confirm('Are you sure')){
+//   console.log('YES');
+// } else {
+//   console.log('NO');
+// }
+
+let val;
+
+// Outer height and width
+val = window.innerHeight;
+val = window.innerWidth;
+
+// Scroll points
+
+val = window.scrollY;
+val = window.scrollX;
+
+// Location Object
+
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+val = window.location.search;
+
+// Redirect
+// window.location.href = 'http://google.com';
+
+// Reload
+
+// window.location.reload();
+
+
+// History Object
+
+console.log(val);
 
 
 
