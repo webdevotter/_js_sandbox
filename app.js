a02eac7b9212063bdec0e64e740807cfa4c2f416
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-14 17:27:59 -0600

    Built In Constructors

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-// Person constructor
-function Person(name, dob) {
-  this.name = name;
-  // this.age = age;
-  this.birthday = new Date(dob);
-  this.calculateAge = function(){
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
+// String
+
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
+
+// name2.foo = 'bar';
+
+// console.log(name2);
+
+console.log(typeof name2);
+
+if(name2 === 'Jeff'){
+  console.log('YES');
+} else {
+  console.log('NO');
 }
 
+// Number
+const num1 = 5;
+const num2 = new Number(5);
 
+console.log(typeof num2);
 
+// Boolean
+const bool1 = true;
+const bool2 = new Boolean(true);
 
+console.log(bool1)
 
+// Function 
 
+const getSum1 = function(x, y){
+  return x + y;
+}
 
-// const brad = new Person('Brad', 39);
-// const john = new Person('John', 30);
+const getSum2 = new Function('x', 'y', 'return x + y');
 
-// console.log(brad.age);
+// Objects 
 
-const brad = new Person('Brad', '9-10-1981');
-console.log(brad.calculateAge());
+const john1 = {name: "John"};
+const john2 = new Object({name: 'John'});
 
+console.log(john2);
+
+// Arrays
+const arr1 = [1, 2, 3, 4];
+const arr2 = new Array(1, 2, 3, 4);
+
+console.log(arr2);
+
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp('\\w+');
+
+console.log(re2);
 
 
 
