64001358f6a4b82d5e8aedf6a2a755b655cf71e6
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-14 18:19:11 -0600

    Prototypal Inheritance

--- app.js
+++ app.js
@@ -1,45 +1,16 @@
-// Object.prototype
-//Person.prototype
-
 // Person constructor
-function Person(firstName, lastName, dob) {
+
+function Person(firstName, lastName){
   this.firstName = firstName;
   this.lastName = lastName;
-  this.birthday = new Date(dob);
-  // this.calculateAge = function(){
-  //   const diff = Date.now() - this.birthday.getTime();
-  //   const ageDate = new Date(diff);
-  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
-  // }
 }
 
-// Calculate age
-Person.prototype.calculateAge = function(){
-  const diff = Date.now() - this.birthday.getTime();
-  const ageDate = new Date(diff);
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
+// Greeting
+Person.prototype.greeting = function(){
+  return `Hello, there ${this.firstName} ${this.lastName}!`;
 }
 
-// Get full name
-Person.prototype.getFullName = function(){
-  return `${this.firstName} ${this.lastName}`;
-}
+const person1 = new Person('John', 'Doe');
 
-// Gets Married
-Person.prototype.getsMarried = function(newLastName){
-  this.lastName = newLastName;
-};
-
-const john = new Person('John' , 'Doe', '9-11-1960');
-const mary = new Person('Mary' , 'Christ', '7-11-1930');
-
-
-
-console.log(john.calculateAge());
-
-mary.getsMarried('Smith');
-
-console.log(mary);
-
-console.log(mary.hasOwnProperty('getFullName'));
+console.log(person1.greeting());
 
