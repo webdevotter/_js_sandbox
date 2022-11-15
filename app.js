682c5d9571a53d0162cd302c62b55e7fcd86962b
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-15 11:22:30 -0600

    ES6 Classes

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-const personPrototypes = {
-  greeting: function() {
-    return `Hello there ${this.firstName} ${this.lastName}`;
-  },
-  getsMarried: function(newLastName) {
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
+
+  greeting() {
+    return `Hello there, ${this.firstName} ${this.lastName}`;
+  }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    return `Hello ${this.firstName} ${this.lastName}. Congratulations, you are ${age} years old today!`;
+  }
+
+  getsMarried(newLastName) {
     this.lastName = newLastName;
+  }
+
+  static addNumbers(x, y) {
+    return x + y;
   }
 }
 
-const mary = Object.create(personPrototypes);
 
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
-mary.age = 30;
-
-
+const mary = new Person('Mary', 'Williams', '11-13-1980');
 mary.getsMarried('Thompson');
-console.log(mary.greeting());
 
-const brad = Object.create(personPrototypes, {
-  firstName: {value: 'Brad' },
-  lastName: {value: 'Traversy'},
-  age: {value: 36}
-});
+console.log(mary.calculateAge());
 
-console.log(brad);
-console.log(brad.greeting());
+console.log(Person.addNumbers(1, 2));
