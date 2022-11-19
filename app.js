c5a3291d6b231035554e10b92b6e5fd60330a019
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-19 10:53:01 -0600

    Data from External API - Chuck Norris
    api mentioned no longer available, but used a different one. Single request.

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(e) {
-  console.log('get jokes');
+  
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET','https://api.chucknorris.io/jokes/random', true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+
+      const output = `<p>${response.value}</p>`
+      
+      document.querySelector('.get-jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
 
   e.preventDefault();
 }
