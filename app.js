ca39b4e42c819c01c1b323653d4b859a639dc02b
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-22 08:23:43 -0600

    The Fetch API
    Text, JSON, External API

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 document.getElementById('button1').addEventListener('click', getText);
 
+document.getElementById('button2').addEventListener('click', getJson);
+
+document.getElementById('button3').addEventListener('click', getExternal);
+
+// Get local text file data
 function getText() {
   fetch('text.txt')
   .then(function(res){
@@ -7,6 +12,45 @@ function getText() {
   })
   .then(function(data){
     console.log(data);
+    document.getElementById('output').innerHTML = data;
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+}
+
+// Get local json data
+function getJson() {
+  fetch('posts.json')
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(data){
+    console.log(data);
+    let output = '';
+    data.forEach(function(post){
+      output += `<li>${post.title}</li>`;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+}
+
+// Get from external API
+function getExternal() {
+  fetch('https://api.github.com/users')
+  .then(function(res){
+    return res.json();
+  })
+  .then(function(data){
+    console.log(data);
+    let output = '';
+    data.forEach(function(user){
+      output += `<li>${user.login}</li>`;
+    });
+    document.getElementById('output').innerHTML = output;
   })
   .catch(function(err){
     console.log(err);
