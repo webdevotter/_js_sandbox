160dd814ca8f1739ebbfc7b519ded03e37822166
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-22 08:38:15 -0600

    Arrow Functions

--- app.js
+++ app.js
@@ -1,58 +1,19 @@
-document.getElementById('button1').addEventListener('click', getText);
+// const sayHello = () => {
 
-document.getElementById('button2').addEventListener('click', getJson);
+//   const action = document.getElementById('action');
+//   action.innerHTML = 'Hello';
+// }
 
-document.getElementById('button3').addEventListener('click', getExternal);
+// One line function doesn't need braces
 
-// Get local text file data
-function getText() {
-  fetch('text.txt')
-  .then(function(res){
-    return res.text();
-  })
-  .then(function(data){
-    console.log(data);
-    document.getElementById('output').innerHTML = data;
-  })
-  .catch(function(err){
-    console.log(err);
-  });
-}
+// const action = document.getElementById('action');
+// const sayHello = () => action.innerHTML = 'Hello';
 
-// Get local json data
-function getJson() {
-  fetch('posts.json')
-  .then(function(res){
-    return res.json();
-  })
-  .then(function(data){
-    console.log(data);
-    let output = '';
-    data.forEach(function(post){
-      output += `<li>${post.title}</li>`;
-    });
-    document.getElementById('output').innerHTML = output;
-  })
-  .catch(function(err){
-    console.log(err);
-  });
-}
 
-// Get from external API
-function getExternal() {
-  fetch('https://api.github.com/users')
-  .then(function(res){
-    return res.json();
-  })
-  .then(function(data){
-    console.log(data);
-    let output = '';
-    data.forEach(function(user){
-      output += `<li>${user.login}</li>`;
-    });
-    document.getElementById('output').innerHTML = output;
-  })
-  .catch(function(err){
-    console.log(err);
-  });
-}
+// Another way
+// const sayHello = () => 'Hello';
+// document.getElementById('action').innerHTML = sayHello();
+
+// Return Object 
+const sayHello = () => ({msg: 'Hello'});
+document.getElementById('action').innerHTML = sayHello().msg;
