6e131b8e175ea597c14c3596b00eca873c41b8b7
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-22 07:43:14 -0600

    The Fetch API

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
-const posts = [
-  {title: 'Post One', body: 'This is post one' },
-  {title: 'Post Two', body: 'This is post two' }
+document.getElementById('button1').addEventListener('click', getText);
 
-];
-
-function createPost() {
-  setTimeout(function() {
-    posts.push(post);
-  }, 2000);
-
+function getText() {
+  fetch('')
 }
-
-function getPosts() {
-  setTimeout(function(){
-    let output = '';
-    posts.forEach(function(){
-      output += `<li>${post.title}</li>`;
-    });
-  }, 1000);
-  document.body.innerHTML = output;
-}
-
-createPost({title: 'Post Three', body: 'This is post three'});
-
-getPosts();
