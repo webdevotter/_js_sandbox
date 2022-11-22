e3fc7ba37bf20ef5930241955b2d4ca75296f21b
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-22 07:58:06 -0600

    The Fetch API

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 document.getElementById('button1').addEventListener('click', getText);
 
 function getText() {
-  fetch('')
+  fetch('text.txt')
+  .then(function(res){
+    return res.text();
+  })
+  .then(function(data){
+    console.log(data);
+  })
+  .catch(function(err){
+    console.log(err);
+  });
 }
