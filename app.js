c62b345d28d98701788f0fcff6c82c2397be6b8c
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-22 08:54:07 -0600

    Arrow Functions

--- app.js
+++ app.js
@@ -15,5 +15,32 @@
 // document.getElementById('action').innerHTML = sayHello();
 
 // Return Object 
-const sayHello = () => ({msg: 'Hello'});
-document.getElementById('action').innerHTML = sayHello().msg;
+// const sayHello = () => ({msg: 'Hello'});
+// document.getElementById('action').innerHTML = sayHello().msg;
+
+// Single param does not need parenthesis
+// const sayHello = firstName => `Hello, ${firstName}.`;
+
+// document.getElementById('action').innerHTML = sayHello('Brad');
+
+// Multiple param does need parenthesis
+// const sayHello = (firstName , lastName) => `Hello, ${firstName} ${lastName}.`;
+
+// document.getElementById('action').innerHTML = sayHello('Brad', 'Smith');
+
+const users = ['Nathan', 'John', 'William'];
+
+// const nameLengths = users.map(function(name){
+//   return name.length;
+// });
+
+// Shorter
+// const nameLengths = users.map((firstName) => {
+//   return firstName.length; 
+// });
+
+//Shortest
+const nameLengths = users.map(firstName => firstName.length);
+
+
+document.getElementById('action').innerHTML = nameLengths;
