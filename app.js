28b6f576ab898589784b5dda35ca50acd47afc50
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-10 17:14:04 -0600

    Reg Expressions Part1
    Evaluation Functions

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
-const user = {email: 'jdoe@gmail.com'}
+let re;
+re = /hello/;
 
-try {
-
-  // Produce a reference error
-  // myFunction();
-
-  //Produce a TypeError
-  // null.myFunction();
-
-  // Will produce a SyntaxError
-  // console.log(eval('2+2'));
-  // eval('Hello World');
-
-  // Will produce a URIError
-  // decodeURIComponent('%');
-  if(!user.name) {
-    // throw 'User has no name';
-    throw new SyntaxError('User has no name');
-  }
-} catch(e) {
-  console.log(`User Error: ${e.message}`);
-  // console.log(e);
-  // console.log(e.name);
-  // console.log(e.message);
-  // console.log(e instanceof ReferenceError);
-  // console.log(e instanceof TypeError);
-} finally {
-  console.log('Finally runs regardless of result...');
-}
-
-console.log('Program continues...');
+console.log(re);
