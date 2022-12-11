9a0dfd40a815b1c1991523310fd2d00e92e7876c
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-11 10:30:48 -0600

    match() search() replace()

--- app.js
+++ app.js
@@ -19,3 +19,18 @@ re = /hello/i; // i = case insensitive
 // const result = re.test('Hello');
 // console.log(result);
 
+// match() - Return result array or null
+// const str = 'Hello There';
+// const result = str.match(re);
+// console.log(result);
+
+// search() - Returns the index of the first match, if not found returns -1
+// const str = 'Brad Hello There';
+// const result = str.search(re);
+// console.log(result);
+
+
+// replace() - Return a new string with some or all matches of a pattern
+const str = 'Hello There';
+const newString = str.replace(re, 'Hi');
+console.log(newString);
