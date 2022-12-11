1ccc1cd41bc05fe4e3a5a02cbb5449f7ae0b0527
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-11 11:03:51 -0600

    Character Sets & Quantifiers

--- app.js
+++ app.js
@@ -12,8 +12,25 @@ re = /h*llo/i;  // Must match any character zero or more times
 re = /gre?a?y/i;  // Must match any character zero or more times - optional characters
 re = /gre?a?y\?/i;  // Escape character
 
+// Brackets [] - Character Sets
+re = /gr[ae]y/i; // Must be an a or e
+re = /[GF]ray/i; // Must be an uppercase G or F
+re = /[^GF]ray/i; // Must be anything except uppercase G or F
+re = /[A-Z]ray/; // Match any uppercase letter
+re = /[a-z]ray/; // Match any lowercase letter
+re = /[A-Za-z]ray/; // Match any letter and case
+re = /[0-9]ray/; // Match any digit
+
+// Braces {} - Quantifiers
+re = /Hel{2}o/i; // Must occur exactly {m} times
+re = /Hel{2,4}o/i; // Must occur between {m,n} times
+re = /Hel{2,}o/i; // Must occur at least {m} times
+
+// Parentheses () - Grouping
+re = /^([0-9]x){3}$/;
+
 // String to match
-const str = 'gray';
+const str = '3x3x3x';
 
 // Log Results
 const result = re.exec(str);
