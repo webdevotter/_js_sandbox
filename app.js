06205e544d28954ed3213918882d669bbebeb274
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-11 10:25:47 -0600

    exec() & test()

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 let re;
 re = /hello/;
+re = /hello/i; // i = case insensitive
+// re = /hello/g; // g = global search
 
-console.log(re);
+// console.log(re);
+// console.log(re.source);
+
+//exec() - Return Result in an Array or Null
+// const result = re.exec('hello world');
+
+// console.log(result);
+
+// console.log(result[0]);
+// console.log(result.index);
+// console.log(result.input);
+
+// test() - Returns true or false
+// const result = re.test('Hello');
+// console.log(result);
+
