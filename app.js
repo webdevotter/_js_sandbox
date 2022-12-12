09306ef23da8bb885dd83e9cb5db41c37d77cf82
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-12 16:09:15 -0600

    ES6 SETS
    Convert sets into arrays
    ForEach Loop
    For...of
    Delete from sett
    Check for values
    Get count

--- app.js
+++ app.js
@@ -8,5 +8,38 @@ set1.add('A string');
 set1.add({name: 'John'});
 set1.add(true);
 
-console.log(set1);
+
+// const set2 = new Set([1, true, 'string']);
+// console.log(set2);
+
+// console.log(set1);
+
+// Get count
+// console.log(set1.size);
+
+// Check for values
+// console.log(set1.has(100));
+// console.log(set1.has(50+50));
+// console.log(set1.has({name: 'John'}));
+
+// Delete from set
+// set1.delete(100);
+
+// console.log(set1);
+
+// ITERATING THROUGH SETS
+
+// For..of
+// for(let item of set1) {
+//  console.log(item);
+// }
+
+// ForEach Loop
+// set1.forEach((value) => {
+//   console.log(value);
+// });
+
+// CONVERT SETS INTO ARRAY
+const setArr = Array.from(set1);
+console.log(setArr);
 
