95af965d1cb2e1ab0a69fbbfa1e800110fa42af1
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-12 15:36:15 -0600

    Array Destructuring

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Destructuring
+// Destructuring Assignment
 
 let a, b;
 [a, b] = [100, 200];
@@ -9,4 +9,20 @@ let a, b;
 ({a, b} = {a:100, b:200, c:300, d:400, e:500});
 ({a, b, ...rest} = {a:100, b:200, c:300, d:400, e:500});
 
-console.log(rest);
+// Array Destructuring 
+
+// const people = ['John', 'Beth', 'Mike'];
+
+// const [person1, person2, person3] = people;
+
+// console.log(person1, person2, person3);
+
+// Parse array returned from function
+function getPeople() {
+  return ['John', 'Beth', 'Mike'];
+}
+
+let person1, person2, person3;
+[person1, person2, person3] = getPeople();
+
+console.log(person1, person2, person3);
