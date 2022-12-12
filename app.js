bb544d620301ca128a3fa68ccb225dadd23e60d1
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-12 06:39:06 -0600

    Generator

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-// Iterator Exampler
+// // Iterator Exampler
 
-function nameIterator(names) {
-  let nextIndex = 0;
+// function nameIterator(names) {
+//   let nextIndex = 0;
 
-  return {
-    next: function() {
-      return nextIndex < names.length ?
-      { value: names[nextIndex++], done: false } :
-      { done: true }
-    }
-  }
+//   return {
+//     next: function() {
+//       return nextIndex < names.length ?
+//       { value: names[nextIndex++], done: false } :
+//       { done: true }
+//     }
+//   }
+// }
+
+// // Create an array of names
+// const namesArr = ['Jack', 'Jill', 'John'];
+
+// // Initialize iterator and pass in the names array
+// const names = nameIterator(namesArr);
+
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+// console.log(names.next());
+
+//Generator Example
+
+function* sayNames() {
+  yield 'Jack';
+  yield 'Jill';
+  yield 'John';
 }
 
-// Create an array of names
-const namesArr = ['Jack', 'Jill', 'John'];
+const name = sayNames();
 
-// Initialize iterator and pass in the names array
-const names = nameIterator(namesArr);
-
-console.log(names.next());
-console.log(names.next());
-console.log(names.next());
-console.log(names.next());
+console.log(name.next().value);
+console.log(name.next().value);
+console.log(name.next().value);
+console.log(name.next().value);
