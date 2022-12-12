590ddf3ed2b180382a711f8ebfc61dd8365491fa
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-12 15:47:40 -0600

    Maps
    Set Keys
    Set map values by key
    Get values by key
    Count values

--- app.js
+++ app.js
@@ -1,54 +1,20 @@
-// Destructuring Assignment
+// MAPS - key-value paris - can use ANY type as a key or value
 
-// let a, b;
-// [a, b] = [100, 200];
+const map1 = new Map();
 
-// //Rest pattern
-// [a, b, ...rest] = [100, 200, 300, 400, 500];
+// Set Keys
+const key1 = 'some string',
+      key2 = {},
+      key3 = function() {};
 
-// ({a, b} = {a:100, b:200, c:300, d:400, e:500});
-// ({a, b, ...rest} = {a:100, b:200, c:300, d:400, e:500});
 
-// Array Destructuring 
+// Set map values by key
+map1.set(key1, 'Value of key1');
+map1.set(key2, 'Value of key2');
+map1.set(key3, 'Value of key3');
 
-// const people = ['John', 'Beth', 'Mike'];
+// Get values by key
+console.log(map1.get(key1), map1.get(key2), map1.get(key3));
 
-// const [person1, person2, person3] = people;
-
-// console.log(person1, person2, person3);
-
-// Parse array returned from function
-// function getPeople() {
-//   return ['John', 'Beth', 'Mike'];
-// }
-
-// let person1, person2, person3;
-// [person1, person2, person3] = getPeople();
-
-// console.log(person1, person2, person3);
-
-// Object Destructuring
-
-const person ={
-  name: 'John Doe',
-  age: 32,
-  city: 'Miami',
-  gender: 'Male',
-  sayHello: function() {
-    console.log('Hello');
-  }
-
-}
-
-// Old ES5
-
-// const name = person.name,
-//           age = person.age,
-//           city = person.city;
-
-// New ES6 Destructuring
-const {name, age, city, sayHello} = person;
-
-console.log(name, age, city);
-
-sayHello();
+// Count values
+console.log(map1.size);
