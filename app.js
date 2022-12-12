0a4c0b4da2a4755f735abd1be44c3c714aa9994b
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-12 15:33:12 -0600

    Destructuring Assignment

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-// Create a symbol
-// const sym1 = Symbol();
-// const sym2 = Symbol('sym2');
+// Destructuring
 
-// console.log(typeof sym2);
+let a, b;
+[a, b] = [100, 200];
 
-// console.log(Symbol() === Symbol()); //can never be the same
-// console.log(`'Hello ${sym1.toString()}'`);
+//Rest pattern
+[a, b, ...rest] = [100, 200, 300, 400, 500];
 
-// Unique Object Keys
-const KEY1 = Symbol();
-const KEY2 = Symbol('sym2');
+({a, b} = {a:100, b:200, c:300, d:400, e:500});
+({a, b, ...rest} = {a:100, b:200, c:300, d:400, e:500});
 
-const myObj = {};
-
-myObj[KEY1] = 'Prop1';
-myObj[KEY2] = 'Prop2';
-myObj.key3 = 'Prop3';
-myObj.key4 = 'Prop4';
-
-console.log(myObj[KEY1]);
-console.log(myObj[KEY2]);
-
-// Symbols are not enumerable in for...in
-// for(let i in myObj) {
-//   console.log(`${i}: ${myObj[i]}`);
-// }
-
-// Symbols are ignored by JSON.stringify
-console.log(JSON.stringify({key: 'prop'}));
-console.log(JSON.stringify({[Symbol('sym1')]: 'prop'}));
+console.log(rest);
