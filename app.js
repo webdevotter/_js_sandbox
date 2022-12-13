5fe296ba04772459d85791ef8bac683baec80710
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-13 16:16:43 -0600

    Revealing Module Pattern

--- app.js
+++ app.js
@@ -27,3 +27,29 @@
 // })();
 
 // UICtrl.callChangeText();
+
+// REVEALING MODULE PATTERN
+const ItemCtrl = (function() {
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log('Item Added...');
+  }
+
+  function get(id) {
+    return data.find(item => {
+      return item.id === id;
+    });
+  }
+
+  return {
+    add: add,
+    get: get
+  }
+
+})();
+
+
+ItemCtrl.add({id: 1, name: 'John'});
+console.log(ItemCtrl.get(1));
