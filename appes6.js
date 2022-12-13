5a88aad0f5d8c2bd0d32088c4b160eb8ec1bd79c
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-13 17:39:35 -0600

    Observer Pattern

--- appes6.js
+++ appes6.js
@@ -1,27 +1,29 @@
-function EventObserver() {
-  this.observers = [];
-}
+class EventObserver {
+    constructor() {
+      this.observers = [];
+    } 
+    
+    subscribe(fn) {
+      this.observers.push(fn);
+      console.log(`You are now subscribed to ${fn.name}`);
+    }
 
-EventObserver.prototype = {
-  subscribe: function(fn) {
-    this.observers.push(fn);
-    console.log(`You are now subscribed to ${fn.name}`);
-  },
-  unsubscribe: function(fn) {
-    this.observers = this.observers.filter(function(item) {
-      if(item !== fn) {
-          return item;
-      }
-    });
-    console.log(`You are now unsubscribed from ${fn.name}`);
-  },
+    unsubscribe(fn) {
+      this.observers = this.observers.filter(function(item) {
+        if(item !== fn) {
+            return item;
+        }
+      });
+      console.log(`You are now unsubscribed from ${fn.name}`);
+    }
 
-  fire: function() {
-    this.observers.forEach(function(item) {
-      item.call();
+    fire() {
+      this.observers.forEach(function(item) {
+        item.call();
     });
   }
 }
+
 
 const click = new EventObserver();
 
