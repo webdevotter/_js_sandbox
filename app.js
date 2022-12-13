cad2213de0432f82ad27f0a658ae24660af9f2cd
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-12-13 16:10:50 -0600

    Standard Module Pattern

--- app.js
+++ app.js
@@ -1,45 +1,29 @@
-// SETS - Store unique values of any type
+// Basic Structure
 
-const set1 = new Set();
+// (function() {
+//   // Declare private vars and functions
 
-// Add value to set
-set1.add(100);
-set1.add('A string');
-set1.add({name: 'John'});
-set1.add(true);
+//   return {
+//     // Declare public variables and functions
+//   }
+// })();
 
 
-// const set2 = new Set([1, true, 'string']);
-// console.log(set2);
+// STANDARD MODULE PATTERN
+// const UICtrl = (function() {
+//   let text = 'hello world';
 
-// console.log(set1);
+//   const changeText = function() {
+//     const element = document.querySelector('h1');
+//     element.textContent = text;
+//   }
 
-// Get count
-// console.log(set1.size);
+//   return {
+//     callChangeText: function() {
+//       changeText();
+//       console.log(text);
+//     }
+//   }
+// })();
 
-// Check for values
-// console.log(set1.has(100));
-// console.log(set1.has(50+50));
-// console.log(set1.has({name: 'John'}));
-
-// Delete from set
-// set1.delete(100);
-
-// console.log(set1);
-
-// ITERATING THROUGH SETS
-
-// For..of
-// for(let item of set1) {
-//  console.log(item);
-// }
-
-// ForEach Loop
-// set1.forEach((value) => {
-//   console.log(value);
-// });
-
-// CONVERT SETS INTO ARRAY
-const setArr = Array.from(set1);
-console.log(setArr);
-
+// UICtrl.callChangeText();
